7de151d994efb969b3dc66009f0a7a2ca6af985f
Paul Standley <paulstandley1972@gmail.com>
2019-06-08 16:42:09 +0100

    Props And Methods

---
+++ 2-props-methods/includes/person.inc.php
@@ -0,0 +1,12 @@
+<?php 
+  class Person {
+
+    public $name;
+    public $eyeColor;
+    public $age;
+
+    public function setName($name) {
+      $this->name = $name;
+    }
+  }
+?>
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## **Paul Standley**
 
 ![Profile Pick](img/profile.png)
+
+__1__ **PHP** CLASS
+
+__2__ **PHP** PROPERTIES AND METHODS
