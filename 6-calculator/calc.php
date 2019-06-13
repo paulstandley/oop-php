844b8f2c8f8b18cd6dd497635a444d350869e597
Paul Standley <paulstandley1972@gmail.com>
2019-06-13 21:01:31 +0100

    finnish calc

--- 6-calculator/calc.php
+++ 6-calculator/calc.php
@@ -1,7 +1,12 @@
 <?php 
 
+  include 'includes/calc.inc.php';
+
   $num1 = $_POST['num1'];
   $num2 = $_POST['num2'];
-  $clac = $_POST['cal'];
+  $calc = $_POST['cal'];
+
+  $calculator = new Calc($num1, $num2, $calc);
+  echo $calculator->calcMethod();
 
 ?>
--- 6-calculator/includes/calc.inc.php
+++ 6-calculator/includes/calc.inc.php
@@ -9,7 +9,25 @@
     public function __construct($num1, $num2, $calc) {
       $this->num1 = $num1;
       $this->num2 = $num2;
-      $this->cal = $cal;
+      $this->calc = $calc;
+    }
+
+    public function calcMethod() {
+      switch ($this->calc) {
+        case 'add':
+          $result = $this->num1 + $this->num2;
+          break;
+        case 'sub':
+          $result = $this->num1 - $this->num2;
+          break;
+        case 'mul':
+          $result = $this->num1 * $this->num2;
+          break;
+        default:
+          $result = "Error";
+          break;
+      }
+      return $result;
     }
 
   }
